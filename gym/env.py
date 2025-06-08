2c431588824b3e1ba72850337d66b1136bbc3143
Danyal <danyalsmail@gmail.com>
2025-06-07 23:30:49 -0400

    Initial structure of env.py with Gym wrapper
    This work was done together over Discord
    
    We decided to structure the Gymnasium env to interface with the
    game's GameState and GameStateData objects. Current goal is to
    get a functioning Pac-Man agent that can play against an A* ghost,
    with room left to select different agents in the future.
    
    Hamza had a great idea to use a 'Master Agent' to control all ghosts
    at once, which would hopefully allow for cooperative strategies
    amongst those agents.
    
    Kaya also spent time writing out our initial objective function scoring
    and the weights attributed to a given action - implementation of that
    for the step() function is our next objective.

--- gym/env.py
+++ gym/env.py
@@ -2,43 +2,155 @@ import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 
-from pacman_engine.pacman import Game, Directions, ClassicGameRules
+from pacman_engine.pacman import Directions, ClassicGameRules, GameState
+from pacman_engine.game import Game, GameStateData, Actions
+from pacman_engine.layout import Layout, get_layout
 
+
+
+""" -------------------- CONSTANTS AND HELPERS -------------------- """
+DIRECTIONS_TO_IDX = {
+    'North': 0,
+    'South': 1,
+    'East': 2,
+    'West': 3,
+    "Northeast": 4,
+    "Northwest": 5,
+    "Southeast": 6,
+    "Southwest": 7
+}
+
+
+
+""" ========================== MAIN CLASS ========================== """
 class PacmanEnv(gym.Env):
-    metadata = {"render_modes": ["pretty", "text"]}
+    """ Gymnasium wrapper around the Berkeley Pac-Man GameState object.
     
-    def __init__(self, layout="mediumClassic", render_mode="pretty"):
+    Parameters:
+        layout:     str - Layout file name (default: "mediumClassic")
+        num_ghosts: int - Maximum number of ghosts that can be spawned from the
+                    layout (default: 4)
+        obs_type:   str - Type of observations the agent can make. 'grid' for a
+                    full board view, 'directional' for an immediate view + directional
+                    view of the ghosts (default: 'grid')
+        training_agent: str - 'pacman' | 'ghost', defines the agent currently being trained
+        ghost_train_idx: int - which ghost agent is being trained, if training_agent == 'ghost'
+        
+                    
+        
+        TODO -  Add selection of which agent is being trained, and default behaviors for non-training agents to
+        TODO -  allow use of trained policy 
+        # ghost_agents: List[str]
+        # pacman_agent: str
+        """
+    metadata = {"render_modes": ["graphics", "text"]}
+    
+    def __init__(self, *, 
+                 layout=        "mediumClassic",
+                 render_mode=   "graphics",
+                 num_ghosts=    4,
+                 obs_type=      "grid",
+                 training_agent=   "pacman",
+                 ghost_train_idx=   1,
+                 ):
+        
         super().__init__()
-        self.layout = layout
+        
+        self.layout_name = layout
         self.render_mode = render_mode
-        self._actions = Directions
-        self.action_space = spaces.Discrete(4)
+        self.num_ghosts = min(max(1, num_ghosts), 4)    # Confine num_ghosts to [1,4]
+        self.obs_type = obs_type
+        self.training_agent = training_agent
+        self.ghost_train_idx = ghost_train_idx
         
-        self.observation_space = spaces.Box(
-            low = 0, high = 255, shape = (84, 84, 3), dtype=np.uint8
-        )
+        # Action space 
+        self._actions = [
+            Directions.NORTH,
+            Directions.SOUTH,
+            Directions.EAST,
+            Directions.WEST,
+            Directions.STOP     # Not sure if we want to allow stopping, but it's
+                                # in the original spec so we can include it for now
+        ]
+        self.action_space = spaces.Discrete(len(self._actions))
         
-        self._start_game()
-        
-    def _start_game(self):
-        self.game = Game("keyboardagent", ["randomghost", 1], ClassicGameRules)
         
         
-    def reset(self, seed=None, options=None):
+        self.reset()
+        
+    """ _________ GYM API _________ """
+    def _reset_game(self):
+        self.state = GameState()
+        layout = get_layout(self.layout_name)
+        self.state.initialize(layout, self.num_ghosts)
+    
+
+    def reset(self, *, seed= None, options: None):
         super().reset(seed=seed)
-        self._start_game()
-        frame = self._render_frame()
-        return frame, {}
+        self._reset_game()
+        return self._make_obs(), {}
     
+    def step(self, action: int):
+        pass
+            
     
-    def step(self, action):
-        dir_str = self._actions[action]
-        reward = self.game.move_pacman(dir_str)
-        done = self.game.game_over
-        frame = self._render_frame()
-        return frame, reward, done, False, {}
+    # TODO - Extend this to be able to render in-terminal using text_display.py
+    def render(self):
+        if self.render_mode != "graphics":
+            return None
     
-    
-    def _render_frame(self):
-        img = self.game.
+
+    def close(self):
+        pass
+        
+        
+    """ ~~~~~~~~~~~ Observation Helpers ~~~~~~~~~~~ """
+        
+    def _build_observation_space(self):
+        layout = get_layout(self.layout_name)
+        height, width = layout.height, layout.width
+        if self.obs_type == "grid":
+            # wall, pellet, power pellet, pacman, ghost, scared ghost
+            self.observation_space = spaces.Box(low=0, high=1, shape=(height, width, 6))
+        else:
+            raise ValueError(f"Unknown obs_type: {self.obs_type}, only option right now is 'grid'")
+        
+    def _make_obs(self):
+        if self.obs_type == 'grid':
+            return self._obs_grid()
+        else:
+            raise ValueError(f"Unknown obs_type: {self.obs_type}, only option right now is 'grid'")
+            
+        
+        
+    def _obs_grid(self):
+        data = self.state.data
+        h, w = data.layout.height, data.layout.width
+        grid = np.zeros((h, w, 6), dtype=int)
+        
+        # Check walls - we want grid[x, y, 0] = data.layout.walls[x][y]
+        grid[:, :, 0] = np.transpose(int(data.layout.walls))
+        
+        # Check pellets
+        grid[:, :, 1] = np.transpose(int(data.layout.food))
+        
+        # Check power pellets
+        grid[:, :, 2] = np.transpose(int(data.capsules))
+        
+        # Check pacman
+        x, y = self.state.get_pacman_position()
+        grid[int(y), int(x), 3] = 1
+        
+        # Check ghost(s) including scared state
+        for ghost in self.state.get_ghost_states():
+            x, y = ghost.get_position()
+            layer = 5 if ghost.scared_timer > 0 else 4
+            grid[int(y), int(x), layer] = 1
+            
+        return grid
+        
+
+        
+            
+            
         
